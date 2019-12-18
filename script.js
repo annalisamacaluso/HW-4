ec6a4c84eeb104907a6b44ae8eeb3cc22b373d13
annalisamacaluso <55858533+annalisamacaluso@users.noreply.github.com>
2019-12-18 15:35:13 -0700

    Add files via upload
    homework upload correction

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
-const questionElement = document.getElementById('title');
+const questionElement = document.getElementById('questions');
 const answerButtonsElement = document.getElementById('answer-buttons');
-//const choicesButtonsElement = document.getElementById('title');
-​
+// const choicesButtonsElement = document.getElementById('title');
+
 let shuffledQuestions, currentQuestionsIndex;
-let score = 0;
-let highScore = +localStorage.getItem("high-score");
-//console.log(typeof highScore);
-highScore = parseInt(highScore);
-​
+
 startButton.addEventListener('click', startGame);
-nextButton.addEventListener('click',function(){
+nextButton.addEventListener('click',()=>{
     currentQuestionsIndex ++;
     setNextQuestion();
 })
-​
+
 function startGame(){
     console.log('started');
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random()- .5);
-    console.log("shuffled questions", shuffledQuestions)
-    currentQuestionsIndex = 0;
-    score = 0;
+    currentQuestionsIndex = 0
     questionContainerElement.classList.remove('hide');
     setNextQuestion()
 }
@@ -32,46 +26,54 @@ function setNextQuestion(){
     resetState();
     showQuestion(shuffledQuestions[currentQuestionsIndex]);
 }
-​
-function showQuestion(question) {
-    console.log(question)
-    questionElement.innerText = question.title;
-    question.choices.forEach(function(choice) {
+
+function showQuestion(questions) {
+    questionElement.innerText = questions.questions;
+    questions.choices.forEach(choices => {
         const button = document.createElement('button')
-        button.innerText = choice;
+        button.innerText = choices.text;
         button.classList.add('btn');
-        button.addEventListener('click', selectAnswer);
-        answerButtonsElement.appendChild(button);
+        if (choices.correct) {
+            button.dataset.correct = choices.correct;
+        }
+        button.addEventListener('click', selectChoices);
+        choicesButtonsElement.appendChild(button);
     })
 }
-​
+
 function resetState(){
     // clearStatusClass(currentQuestionsIndex);
     nextButton.classList.add('hide');
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    while (choicesButtonsElement.firstChild) {
+        choicesButtonsElement.removeChild(choicesButtonsElement.firstChild);
     }
 }
-​
-function selectAnswer(e){
+
+function selectAnswer(){
     const selectedButton = e.target;
-    console.log("selBut", selectedButton)
-  
-    if(selectedButton.innerText === shuffledQuestions[currentQuestionsIndex].answer){
-        score++
-    } else {
-​
-    }
-​
+    const correct = selectedButton.dataset.correctl
+    setStatusClass(document.body,correct);
+    Array.from(choicesButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    })
     if(shuffledQuestions.length > currentQuestionsIndex +1){
         nextButton.classList.remove('hide');
     } else {
-        //when the game is over
         startButton.innerText  = 'Restart';
         startButton.classList.remove('hide');
-​
-        if(score > highScore){
-            localStorage.setItem("high-score", score)
-        }
     }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    }else{
+        element.classList.add('wrong');
+    }     
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
